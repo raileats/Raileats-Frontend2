8983af90305ba095ad0499dd91e548817735c668
raileats <raileats@gmail.com>
2025-08-22 23:54:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import HeroBanner from "./components/HeroBanner";
 import SearchTabs from "./components/SearchTabs";
-import PopularItems from "./components/PopularItem"; // ✅ Corrected
+import PopularItems from "./components/PopularItems"; // ✅ Now correct file name
 import Footer from "./components/Footer";
 import LoginModal from "./components/LoginModal";
 import OrderHistory from "./components/OrderHistory";
@@ -15,32 +15,24 @@ function App() {
     setShowLogin(false);
     if (localStorage.getItem("customerName")) {
       setIsLoggedIn(true);
-      setShowOrderHistory(true); // ✅ Redirect to OrderHistory after login
+      setShowOrderHistory(true);
     }
   };
 
   return (
     <div className="bg-white min-h-screen text-black">
-      {/* Banner */}
       <HeroBanner />
-
-      {/* Search Tabs */}
       <SearchTabs />
 
-      {/* Show OrderHistory if logged in */}
       {showOrderHistory ? (
         <OrderHistory />
       ) : (
         <>
-          {/* Popular Items */}
           <PopularItems />
-
-          {/* Footer with Login button */}
           <Footer onLoginClick={() => setShowLogin(true)} />
         </>
       )}
 
-      {/* Login Modal */}
       <LoginModal show={showLogin} onClose={handleLoginClose} />
     </div>
   );
